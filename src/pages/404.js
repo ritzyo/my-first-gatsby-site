219ff47ca5c470b4af6492135d669e6c0eb5cb42
Ritesh Thakur <ritz.thakur@gmail.com>
2025-05-28 21:20:16 -0500

    added styles folder, moved 404 styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,45 +1,25 @@
 import * as React from "react"
 import { Link } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import "../styles/404.css"
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
+    <main className="main">
+      <h1 className="page-title">Page not found</h1>
+      <p className="paragraph">
+        Sorry 😔, we couldn't find what you were looking for.
         <br />
         {process.env.NODE_ENV === "development" ? (
           <>
             <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
+            Try creating a page in <code className="code-block">src/pages/</code>.
             <br />
           </>
         ) : null}
         <br />
         <Link to="/">Go home</Link>.
       </p>
+      <h1 className="secondary-title">Another Title</h1>
     </main>
   )
 }
